15e88d4b23bdc1e5073b4924c66ac27b6ebba9ca
ochazyrli <ochazyrli@gmail.com>
2021-04-23 21:08:21 +0700

    Tugas-1

---
+++ Tugas-1/soal-2-pakebonus.py
@@ -0,0 +1,9 @@
+#Pelatihan Basic Python Indonesia.Ai
+#Zyrlirosa - 081287980257
+#Soal 2 + Bonus
+
+phi = 22/7
+r = int(input("Jari - jari lingkaran :"))
+luas_lingkaran = (phi*r*r)
+
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2".format(r,luas_lingkaran))
