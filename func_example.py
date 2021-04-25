64683238dd72c5f1f6f70ad87d60847f7384429d
ochazyrli <ochazyrli@gmail.com>
2021-04-25 14:29:18 +0700

    func_example.py

--- func_example.py
+++ func_example.py
@@ -2,11 +2,11 @@ import math
 
 def luas_lingkaran(r):
     luas = math.pi*r*r
-    print("Luas Lingkaran dengan jari jari {} adalah : {:.2f} cm\u00b2".format(r,luas))
+    print("Luas Lingkaran dengan jari jari {} cm adalah : {:.2f} cm\u00b2".format(r,luas))
 
 def keliling_lingkaran(r):
     keliling = 2*math.pi*r
-    print("Keliling Lingkaran dengan jari jari {} adalah : {:.2f} cm\u00b2".format(r,keliling))
+    print("Keliling Lingkaran dengan jari jari {} cm adalah : {:.2f} cm\u00b2".format(r,keliling))
 
 r = int(input("Jari - jari lingkaran : "))
 
